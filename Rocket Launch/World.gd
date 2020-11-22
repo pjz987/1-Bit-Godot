36aaab9bbfc7a1a9569dd7322153fe027deb5a2d
Pete Jones <pwj2012@gmail.com>
2020-11-22 13:12:14 -0800

    extra: add unused launch sprite

--- Rocket Launch/World.gd
+++ Rocket Launch/World.gd
@@ -3,5 +3,10 @@ extends Node
 onready var animationPlayer: = $AnimationPlayer # inferred strict typing
 #onready var animationPlayer: AnimationPlayer = $AnimationPlayer # strict typing
 
+#$RocketShipLaunch.visible = false
+
 func _on_LaunchButton_pressed():
 	animationPlayer.play("Launch")
+	$RocketShip.visible = false
+	$RocketShipLaunch.visible = true
+	
