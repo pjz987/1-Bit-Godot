86dfb262a254de7a09362db8e592089dcd730db6
Pete Jones <pwj2012@gmail.com>
2020-11-22 12:50:49 -0800

    TextureButton Node + signals

--- Rocket Launch/World.gd
+++ Rocket Launch/World.gd
@@ -3,5 +3,5 @@ extends Node
 onready var animationPlayer: = $AnimationPlayer # inferred strict typing
 #onready var animationPlayer: AnimationPlayer = $AnimationPlayer # strict typing
 
-func _ready():
+func _on_LaunchButton_pressed():
 	animationPlayer.play("Launch")
