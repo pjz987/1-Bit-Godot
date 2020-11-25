69c432ee4247c422f2ca7dfbc39b9411418ee02c
Pete Jones <pwj2012@gmail.com>
2020-11-25 06:12:41 -0800

    Animated Character + Review

--- Metroidvania/Player.gd
+++ Metroidvania/Player.gd
@@ -7,6 +7,9 @@ export (int) var GRAVITY = 200
 export (int) var JUMP_FORCE = 128
 export (int) var MAX_SLOPE_ANGLE = 46
 
+onready var sprite = $Sprite
+onready var spriteAnimator = $SpriteAnimator
+
 var motion = Vector2.ZERO
 
 func _physics_process(delta):
@@ -15,6 +18,7 @@ func _physics_process(delta):
 	apply_friction(input_vector)
 	jump_check()
 	apply_gravity(delta)
+	update_animations(input_vector)
 	move()
 
 func get_input_vector():
@@ -35,12 +39,23 @@ func jump_check():
 	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
 		motion.y = -JUMP_FORCE
 		motion.y = min(motion.y, JUMP_FORCE)
-	elif Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
+		
+	if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
 		motion.y = -JUMP_FORCE / 2
 
 func apply_gravity(delta):
 #	if !is_on_floor():
 		motion.y += GRAVITY * delta
+
+func update_animations(input_vector):
+	if input_vector.x != 0:
+		sprite.scale.x = sign(input_vector.x)
+		spriteAnimator.play("Run")
+	else:
+		spriteAnimator.play("Idle")
+		
+	if !is_on_floor():
+		spriteAnimator.play("Jump")
 
 func move():
 	motion = move_and_slide(motion, Vector2.UP)
