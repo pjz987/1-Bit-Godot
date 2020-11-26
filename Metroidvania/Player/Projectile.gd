939c86805cc7f40490df1bd9dfe41165aca82ae9
Pete Jones <pwj2012@gmail.com>
2020-11-26 10:56:46 -0800

    Hurtboxes + Hitboxes

--- Metroidvania/Player/Projectile.gd
+++ Metroidvania/Player/Projectile.gd
@@ -9,3 +9,11 @@ func _process(delta):
 
 func _on_VisibilityNotifier2D_viewport_exited(_viewport):
 	queue_free()
+
+
+func _on_Hitbox_body_entered(body):
+	queue_free()
+
+
+func _on_Hitbox_area_entered(area):
+	queue_free()
