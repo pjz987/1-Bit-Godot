418516ae7c9a3dd30389c3d2922099d9d2d63e9b
Pete Jones <pwj2012@gmail.com>
2020-11-26 13:55:16 -0800

    Enemy Stats + Review

--- Metroidvania/Player/Projectile.gd
+++ Metroidvania/Player/Projectile.gd
@@ -13,11 +13,11 @@ func _on_VisibilityNotifier2D_viewport_exited(_viewport):
 	queue_free()
 
 
-func _on_Hitbox_body_entered(body):
+func _on_Hitbox_body_entered(_body):
 	Utils.instance_scene_on_main(ExplosionEffect, global_position)
 	queue_free()
 
 
-func _on_Hitbox_area_entered(area):
+func _on_Hitbox_area_entered(_area):
 	Utils.instance_scene_on_main(ExplosionEffect, global_position)
 	queue_free()
