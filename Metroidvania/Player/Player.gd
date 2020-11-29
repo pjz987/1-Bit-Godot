2d67f9a1503c83597af895ec5b56ae00a33e2634
Pete Jones <pwj2012@gmail.com>
2020-11-29 10:09:33 -0800

    Saver + Loader

--- Metroidvania/Player/Player.gd
+++ Metroidvania/Player/Player.gd
@@ -88,6 +88,15 @@ func _physics_process(delta):
 		if PlayerStats.missiles and PlayerStats.missiles_unlocked:
 			fire_missile()
 
+func save():
+	var save_dictionary = {
+		filename = get_filename(),
+		parent = get_parent().get_path(),
+		position_x = position.x,
+		position_y = position.y
+	}
+	return save_dictionary
+
 func fire_bullet():
 	var bullet = Utils.instance_scene_on_main(PlayerBullet, muzzle.global_position)
 	bullet.velocity = Vector2.RIGHT.rotated(gun.rotation) * BULLET_SPEED
