806305dd07bc7a25d4a9eeb02d59670e1b25effc
Pete Jones <pwj2012@gmail.com>
2020-11-29 17:29:25 -0800

    Level Building + Review

--- Metroidvania/World/SaveStation.gd
+++ Metroidvania/World/SaveStation.gd
@@ -1,7 +1,11 @@
 extends StaticBody2D
 
+var PlayerStats = ResourceLoader.PlayerStats
+
 onready var animationPlayer = $AnimationPlayer
 
 func _on_SaveArea_body_entered(_body):
+#	PlayerStats.health = PlayerStats.max_health
 	animationPlayer.play("Save")
 	SaverAndLoader.save_game()
+	PlayerStats.refill_stats()
